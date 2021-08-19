ba631925c651adbaa8f7701de2a9f8b6c57f2949
Martin Jablecnik <martin.jablecnik@email.cz>
2021-08-19 09:26:51 +0200

    Add scrolling for LoginPage

--- lib/app/modules/auth/ui/login_page.dart
+++ lib/app/modules/auth/ui/login_page.dart
@@ -21,91 +21,96 @@ class _LoginPageState extends ModularState<LoginPage, UserListStore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Spacer(),
-              Center(
-                child: Text(
-                  "Login",
-                  style: GoogleFonts.cookie(fontSize: 80.0),
-                ),
-              ),
-              SizedBox(height: 24.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                ),
-                validator: FieldValidator.email(),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                enableSuggestions: false,
-                autocorrect: false,
-                obscureText: true,
-                initialValue: "Test123",
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                  suffixIcon: Icon(Icons.visibility),
-                ),
-                validator: FieldValidator.password(
-                  minLength: 8,
-                  shouldContainNumber: true,
-                  shouldContainCapitalLetter: true,
-                  shouldContainSmallLetter: true,
-                  errorMessage: "Password must have minimal 8 characters",
-                  onNumberNotPresent: () => "Password must contain number",
-                  onSpecialCharsNotPresent: () => "Password must contain special characters",
-                  onCapitalLetterNotPresent: () => "Password must contain capital letters",
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    TextButton(
-                      onPressed: () => print("TODO: Go to forgot password page."),
-                      child: Text("Forgot password?"),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: IntrinsicHeight(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Spacer(),
+                  Center(
+                    child: Text(
+                      "Login",
+                      style: GoogleFonts.cookie(fontSize: 80.0),
                     ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Processing Data'),
-                              ),
-                            );
-                          }
-                        },
-                        child: Text('Login'),
-                      ),
+                  ),
+                  SizedBox(height: 24.0),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
                     ),
-                  ],
-                ),
+                    validator: FieldValidator.email(),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    initialValue: "Test123",
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                      suffixIcon: Icon(Icons.visibility),
+                    ),
+                    validator: FieldValidator.password(
+                      minLength: 8,
+                      shouldContainNumber: true,
+                      shouldContainCapitalLetter: true,
+                      shouldContainSmallLetter: true,
+                      errorMessage: "Password must have minimal 8 characters",
+                      onNumberNotPresent: () => "Password must contain number",
+                      onSpecialCharsNotPresent: () => "Password must contain special characters",
+                      onCapitalLetterNotPresent: () => "Password must contain capital letters",
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        TextButton(
+                          onPressed: () => print("TODO: Go to forgot password page."),
+                          child: Text("Forgot password?"),
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Processing Data'),
+                                  ),
+                                );
+                              }
+                            },
+                            child: Text('Login'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?"),
+                        TextButton(
+                          onPressed: () => print("TODO: Go to register page."),
+                          child: Text("Register!"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              Spacer(),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account?"),
-                    TextButton(
-                      onPressed: () => print("TODO: Go to register page."),
-                      child: Text("Register!"),
-                    ),
-                  ],
-                ),
-              )
-            ],
+            ),
           ),
         ),
       ),
